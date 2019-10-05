726710fc59940332bd108ac29e23502a2fb0127c
oonqt <r0blox1053@outlook.com>
2019-10-05 11:31:14 -0700

    save settings, send dom content

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const { app, BrowserWindow, dialog, ipcMain } = require("electron");
 const path = require("path");
-const logger = require("./utils/logger");
+const Logger = require("./utils/logger");
 const startupHandler = require("./utils/startupHandler");
+const JsonDB = require("./utils/JsonDB");
 const RPCClient = require("./utils/RPCClient");
 const { version, homepage, author, license } = require("./package.json");
 
+const db = new JsonDB(path.join(app.getPath("userData"), "config.json"));
+const startup = new startupHandler(app);
+const logger = new Logger((process.defaultApp ? "console" : "file"), app.getPath("userData"));
+
 let mainWindow;
+let rpc;
 
 function startApp() {
     mainWindow = new BrowserWindow({
@@ -19,8 +25,11 @@ function startApp() {
         show: false
     });
 
+    // mainWindow.setMenu(null);
+
     mainWindow.webContents.on("dom-ready", () => {
-        mainWindow.webContents.send("data:info", { version, homepage, author, license })
+        mainWindow.webContents.send("data:info", { version, homepage, author, license });
+        mainWindow.webContents.send("data:settings", db.data);
     });
 
     mainWindow.once("ready-to-show", () => {
@@ -30,7 +39,15 @@ function startApp() {
     mainWindow.loadFile(path.join("public", "index.html"));
 }
 
+ipcMain.on("settings:app", ({}, data) => {
+    db.save(data);
+    dialog.showMessageBox(mainWindow, { type: "info", detail: "Saved settings" });
+});
 
+ipcMain.on("settings:appearance", ({}, data) => {
+    db.save(data);
+    dialog.showMessageBox(mainWindow, { type: "info", detail: "Saved settings" });
+});
 
 app.once("ready", startApp);
 
